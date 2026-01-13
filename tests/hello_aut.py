ec412727c75f414c6b85a15df72944d21b02bcd1
gr0zhx <139311001+gr0zhx@users.noreply.github.com>
2026-01-13 20:18:57 +0700

    Fix 3 Browser

--- tests/hello_aut.py
+++ tests/hello_aut.py
@@ -5,26 +5,27 @@ from selenium.webdriver.common.by import By
 class AutTest(unittest.TestCase):
 
     def setUp(self):
-        options = webdriver.FirefoxOptions()
-        options.add_argument('--ignore-ssl-errors=yes')
-        options.add_argument('--ignore-certificate-errors')
-        server = 'http://localhost:4444'
+        browser = sys.argv[2] if len(sys.argv) > 2 else "firefox"   #(ambil browser dari matrix)
+        server = "http://localhost:4444"                            #(alamat Selenium Hub)
+
+        options = None
+        if browser == "chrome":
+            options = webdriver.ChromeOptions()
+        elif browser == "edge":
+            options = webdriver.EdgeOptions()
+        else:
+            options = webdriver.FirefoxOptions()
 
         self.browser = webdriver.Remote(command_executor=server, options=options)
-        self.addCleanup(self.browser.quit)
+        self.addCleanup(self.browser.quit)                          #(tutup browser setelah test)
 
     def test_homepage(self):
-        if len(sys.argv) > 1:
-            url = sys.argv[1]
-        else:
-            url = "http://docker-apache"
+        url = sys.argv[1] if len(sys.argv) > 1 else "http://docker-apache"  #(alamat AUT)
+        self.browser.get(url)                                         #(buka AUT)
+        expected = "Welcome back, Guest!"                             #(teks yang diharapkan)
+        actual = self.browser.find_element(By.TAG_NAME, 'p').text     #(ambil isi <p>)
+        self.browser.save_screenshot('ss.png')                #(ambil screenshot)
+        self.assertIn(expected, actual)                               #(validasi hasil)
 
-        self.browser.get(url)
-        self.browser.save_screenshot('ss.png')    
-        expected_result = "Welcome back, Guest!"
-        actual_result = self.browser.find_element(By.TAG_NAME, 'p')
-
-        self.assertIn(expected_result, actual_result.text)
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main(argv=['first-arg-is-ignored'], verbosity=2, warnings='ignore')
